3a65f1c132b762afb1ffa30860211b68fed68265
Jhalak Verma <jhalakmverma.15@gmail.com>
2025-04-17 16:57:15 +0530

    Add files via upload

---
+++ gui.py
@@ -0,0 +1,39 @@
+import streamlit as st
+import tensorflow as tf
+import numpy as np
+import cv2
+from PIL import Image
+
+@st.cache_resource
+def load_model():
+    model = tf.keras.models.load_model("/Users/jhalakverma/Desktop/Monument/trained_model.h5")
+
+model = load_model()
+
+class_names = ['Ajanta Caves', 'Charar-E- Sharif', 'Chhota_Imambara',
+       'Ellora Caves', 'Fatehpur Sikri', 'Gateway of India',
+       'Humayun_s Tomb', 'India gate pics', 'Khajuraho',
+       'Sun Temple Konark', 'alai_darwaza', 'alai_minar',
+       'basilica_of_bom_jesus', 'charminar', 'golden temple',
+       'hawa mahal pics', 'iron_pillar', 'jamali_kamali_tomb',
+       'lotus_temple', 'mysore_palace', 'qutub_minar', 'tajmahal',
+       'tanjavur temple', 'victoria memorial']
+
+st.title("Indian Monument Classifier ")
+st.write("Upload an image of a monument and the model will try to identify it.")
+
+uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
+
+if uploaded_file is not None:
+    image = Image.open(uploaded_file)
+    st.image(image, caption="Uploaded Image", use_column_width=True)
+
+    img = np.array(image)
+    img = cv2.resize(img, (224, 224))
+    img = img / 255.0
+    img = np.expand_dims(img, axis=0)
+
+    predictions = model.predict(img)
+    predicted_class = class_names[np.argmax(predictions)]
+
+    st.success(f"Predicted Monument: **{predicted_class}**")
