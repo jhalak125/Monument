6d9beb3635a25d2286d450894492c8fddd3d860c
Jhalak Verma <jhalakmverma.15@gmail.com>
2025-04-17 17:11:06 +0530

    Update gui.py

--- gui.py
+++ gui.py
@@ -1,12 +1,12 @@
 import streamlit as st
 import tensorflow as tf
 import numpy as np
-import cv2
 from PIL import Image
 
 @st.cache_resource
 def load_model():
     model = tf.keras.models.load_model("/Users/jhalakverma/Desktop/Monument/trained_model.h5")
+    return model
 
 model = load_model()
 
@@ -25,12 +25,11 @@ st.write("Upload an image of a monument and the model will try to identify it.")
 uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
 
 if uploaded_file is not None:
-    image = Image.open(uploaded_file)
+    image = Image.open(uploaded_file).convert("RGB")
     st.image(image, caption="Uploaded Image", use_column_width=True)
 
-    img = np.array(image)
-    img = cv2.resize(img, (224, 224))
-    img = img / 255.0
+    img = image.resize((224, 224))
+    img = np.array(img) / 255.0
     img = np.expand_dims(img, axis=0)
 
     predictions = model.predict(img)
